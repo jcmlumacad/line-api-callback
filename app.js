4f001a4705946877c3f034d27934840666a149f9
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-01-23 12:32:00 +0800

    test

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var express = require('express'),
     app = express(),
     port = 3000;
 
+app.get('/', function (req, res) {
+    console.log('Test');
+});
+
 app.post('/callback', function (req, res) {
     console.log(res);
 });
