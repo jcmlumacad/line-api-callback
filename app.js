f52014ef685d43654ac436b3818a736efbd18304
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-01-23 13:41:31 +0800

    change console

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require('express'),
     port = process.env.PORT || 5000;
 
 app.post('/callback', function (req, res) {
-    console.log(res);
+    console.log(req);
 });
 
 app.listen(port, function () {
