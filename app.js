0e6e565cfd478c0238cb655f4e17b90e66744b36
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-01-23 12:18:05 +0800

    initiate

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var express = require('express'),
+    app = express(),
+    port = 3000;
+
+app.post('/callback', function (req, res) {
+    console.log(res);
+});
+
+app.listen(port);
+console.log('Listen to port:', port);
