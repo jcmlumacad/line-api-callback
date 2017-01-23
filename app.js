1adec65c7d96e26738af6597a1f427ac0bf90103
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-01-23 13:56:44 +0800

    modified changes

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 'use strict';
 
 var express = require('express'),
+    bodyParser = require('body-parser'),
     app = express(),
     port = process.env.PORT || 5000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.post('/callback', function (req, res) {
-    console.log(req);
+    console.log(req.body);
 });
 
 app.listen(port, function () {
